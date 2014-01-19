27d8e09fd0823d20f7e78b30cb603bc1bb903449
Toon Schoenmakers <nighteyes1993@gmail.com>
2014-01-19 22:26:16 +0100

    Updated documentation
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- search/all_8.js
+++ search/all_8.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['initcrashhandler',['InitCrashHandler',['../namespacetrippingcyril_1_1crash.html#a1de8b6a3a20b0945914f0f7bbdde1687',1,'trippingcyril::crash']]],
-  ['isconnected',['IsConnected',['../classtrippingcyril_1_1_socket.html#a75f46c7625515bfa0c20799901c7d3d2',1,'trippingcyril::Socket']]],
+  ['isconnected',['IsConnected',['../classtrippingcyril_1_1_socket.html#a1f91089259245f5211f99d440d69b90d',1,'trippingcyril::Socket']]],
   ['isopen',['IsOpen',['../classtrippingcyril_1_1_file.html#a128a8f2f324a5ebd907b507b2a26f70f',1,'trippingcyril::File']]]
 ];
