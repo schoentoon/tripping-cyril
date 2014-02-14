da86779ee245def54c44119372c6f6d606770641
Toon Schoenmakers <nighteyes1993@gmail.com>
2014-02-14 14:54:54 +0100

    Updated documentation
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- search/all_13.js
+++ search/all_13.js
@@ -1,6 +1,7 @@
 var searchData=
 [
   ['wait',['Wait',['../classtrippingcyril_1_1_cond_var.html#a3ea047100c8480cbfbc31defb9237556',1,'trippingcyril::CondVar']]],
+  ['wantsthread',['wantsThread',['../classtrippingcyril_1_1_module.html#aec705041fb9499e02b51dfb76afc3eaf',1,'trippingcyril::Module']]],
   ['wildcmp',['WildCmp',['../classtrippingcyril_1_1_string.html#a7c1936ed503e645704fcf8017d79b9e3',1,'trippingcyril::String']]],
   ['write',['Write',['../classtrippingcyril_1_1_file.html#ade7c3db03f4b1cd83fc1d710bf8ae43e',1,'trippingcyril::File::Write(const char *buffer, size_t len)'],['../classtrippingcyril_1_1_file.html#ae09df87a6cf92214501e6d3ee7f8cf10',1,'trippingcyril::File::Write(const String &amp;data)'],['../classtrippingcyril_1_1_pipe.html#aa11631b710c59b1c75b3035d2b101abe',1,'trippingcyril::Pipe::Write(const char *buffer, size_t len)'],['../classtrippingcyril_1_1_pipe.html#a71ae93f9fc73441cafbbbdd230d67992',1,'trippingcyril::Pipe::Write(const String &amp;data)'],['../classtrippingcyril_1_1_socket.html#af5a521b0181babcfa236949424adf2f9',1,'trippingcyril::Socket::Write(const char *data, size_t len)'],['../classtrippingcyril_1_1_socket.html#ab7599a28af2ce993da1bb1457f6d8bd0',1,'trippingcyril::Socket::Write(const String &amp;data)']]]
 ];
--- search/search.js
+++ search/search.js
@@ -12,7 +12,7 @@ var indexSectionsWithContent =
   2: "t",
   3: "dfgjlmpst",
   4: "abcdefghijlmopqrstuw~",
-  5: "admrst",
+  5: "admrstw",
   6: "m",
   7: "f",
   8: "bcdflrstu",
