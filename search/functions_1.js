7fff911a44e9b80a15c7b9216dc78d21eeee4e05
Toon Schoenmakers <nighteyes1993@gmail.com>
2014-02-19 17:27:05 +0100

    Updated documentation
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- search/functions_1.js
+++ search/functions_1.js
@@ -1,4 +1,6 @@
 var searchData=
 [
+  ['base64decode',['Base64Decode',['../classtrippingcyril_1_1_string.html#a282af5b5720df2bbd62870e0eb302f28',1,'trippingcyril::String']]],
+  ['base64encode',['Base64Encode',['../classtrippingcyril_1_1_string.html#af94268c74b7b90dc3729eb3e72a10d23',1,'trippingcyril::String']]],
   ['blockingpostgres',['BlockingPostGres',['../classtrippingcyril_1_1_blocking_post_gres.html#ae715832d1f9320017e15d177ff7c986d',1,'trippingcyril::BlockingPostGres']]]
 ];
