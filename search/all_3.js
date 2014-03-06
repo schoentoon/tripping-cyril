9b1855bc302b94a3c39c3baeb5ad4e73a63dc47d
Toon Schoenmakers <nighteyes1993@gmail.com>
2014-03-06 14:21:12 +0100

    Updated documentation
    Signed-off-by: Toon Schoenmakers <nighteyes1993@gmail.com>

--- search/all_3.js
+++ search/all_3.js
@@ -14,10 +14,11 @@ var searchData=
   ['defines_2eh',['defines.h',['../defines_8h.html',1,'']]],
   ['delete',['Delete',['../classtrippingcyril_1_1_file.html#a5fb487ebd77ae80f9987a490e97033f3',1,'trippingcyril::File']]],
   ['detach',['Detach',['../classtrippingcyril_1_1_thread.html#a304092a2bc70f45c573acf7b87a15a4e',1,'trippingcyril::Thread']]],
-  ['dir',['Dir',['../classtrippingcyril_1_1_dir.html',1,'trippingcyril']]],
   ['dir',['Dir',['../classtrippingcyril_1_1_dir.html#a2f31a058b92a77b08f39637f4a0a7a02',1,'trippingcyril::Dir']]],
+  ['dir',['Dir',['../classtrippingcyril_1_1_dir.html',1,'trippingcyril']]],
   ['directory',['DIRECTORY',['../classtrippingcyril_1_1_file.html#af8f8c6ba3c0706c001186e9a30cece66a27f5f1a5956ada593fd0b9b835f9243f',1,'trippingcyril::File']]],
   ['disableautocommit',['disableAutoCommit',['../classtrippingcyril_1_1_database.html#a21e6e7f52aaaff4117d7a0735bf3a948',1,'trippingcyril::Database']]],
   ['disabletimeout',['DisableTimeout',['../classtrippingcyril_1_1_socket.html#afd236ae873bae17551b08d098f02efad',1,'trippingcyril::Socket']]],
-  ['disconnected',['Disconnected',['../classtrippingcyril_1_1_simple_h_t_t_p_socket.html#a79d4c405e401441b5e1c352eea36dd8f',1,'trippingcyril::SimpleHTTPSocket::Disconnected()'],['../classtrippingcyril_1_1_socket.html#abbc799120fa62b9fed7455deb1401b06',1,'trippingcyril::Socket::Disconnected()']]]
+  ['disconnected',['Disconnected',['../classtrippingcyril_1_1_simple_h_t_t_p_socket.html#a79d4c405e401441b5e1c352eea36dd8f',1,'trippingcyril::SimpleHTTPSocket::Disconnected()'],['../classtrippingcyril_1_1_socket.html#abbc799120fa62b9fed7455deb1401b06',1,'trippingcyril::Socket::Disconnected()']]],
+  ['dry_5frun',['DRY_RUN',['../classtrippingcyril_1_1_statsd_client.html#a77daa15b58efe24ec47605689d3ee146',1,'trippingcyril::StatsdClient']]]
 ];
